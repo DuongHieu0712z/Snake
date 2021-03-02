240e36bf4f4dcc72d2cf1ab4d75dd917250dedeb
DuongHieu <duonghieu0712z@gmail.com>
2021-03-02 22:50:06 +0700

    Update Game.py

--- Game.py
+++ Game.py
@@ -10,7 +10,6 @@ from Food import Food
 class Game:
     def __init__(self) -> None:
         pygame.init()
-        pygame.mixer.init()
 
         self.screen = pygame.display.set_mode(SCREEN)
         pygame.display.set_caption('Snake')
@@ -44,9 +43,8 @@ class Game:
 
         if self.snake.isCollisionBody():
             self.is_running = False
-
-        text = f'Score:{self.score}'
-        self.score_board.setText(text)
+            
+        self.score_board.setScore(self.score)
 
     def render(self) -> None:
         self.screen.fill(BLACK)
@@ -69,10 +67,9 @@ class Game:
             lag += time_past
 
             self.handleEvent()
-            while lag > ms_per_frame:
+            while lag >= ms_per_frame:
                 self.update()
                 lag -= ms_per_frame
             self.render()
 
         pygame.quit()
-        pygame.mixer.quit()
