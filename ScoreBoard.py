c3bc37d071dc80651fc8cbc59eb0866815f94fb3
DuongHieu <duonghieu0712z@gmail.com>
2021-03-02 18:21:51 +0700

    Update ScoreBoard.py

--- ScoreBoard.py
+++ ScoreBoard.py
@@ -3,15 +3,15 @@ from header import *
 
 
 class ScoreBoard:
-    def __init__(self, pos: tuple, width: int, height: int) -> None:
-        self.pos = pygame.Vector2(pos)
-        self.surface = pygame.Surface((width, height))
-        self.width = width
-        self.height = height
+    def __init__(self) -> None:
+        self.pos = pygame.Vector2(SCORE_BOARD_POS)
+        self.width = SCORE_BOARD_WIDTH
+        self.height = SCORE_BOARD_HEIGHT
+        self.surface = pygame.Surface((self.width, self.height))
 
         self.font = pygame.font.SysFont('consolas', 16)
         self.font.bold = True
-        self.text = pygame.Surface((width, height))
+        self.text = pygame.Surface((0, 0))
 
     def setText(self, text: str) -> None:
         self.text = self.font.render(text, True, WHITE)
